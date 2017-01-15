f50d49f2550947328377b3a3cdb006acdbace8f0
Bailey Thompson <bailey.kyle.thompson@gmail.com>
2017-01-15 15:33:34 -0500

    Update for Javadoc.

--- src/categoricalsyllogism/CategoricalSyllogism.java
+++ src/categoricalsyllogism/CategoricalSyllogism.java
@@ -1,15 +1,8 @@
-/**
- * Bailey Thompson
- * Categorical Syllogism (1.0.2)
- * 15 January 2016
- * Info: Randomly  generates  a  categorical  syllogism and the three letters and one number which describes it. It also
- * Info: states  whether the syllogism is valid or invalid. As such, if a fallacy is committed, the fallacy will be red,
- * Info: if  the  fallacy  is  not  committed, it will be green. It is also possible to add words which will be randomly
- * Info: generated  in  the  statements.  Last but not least, the program has a venn diagram. Check marks will appear as
- * Info: green, x marks will appear as red, and filled in sections will be filled in with orange.
- */
 package categoricalsyllogism;
 
+/**
+ * Entry point.
+ */
 public class CategoricalSyllogism {
 
     public static void main(String[] args) {
