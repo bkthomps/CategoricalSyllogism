40a1f9614b9f56f3dd8a5e4460c5905e378e46d2
Bailey Thompson <bailey.kyle.thompson@gmail.com>
2017-02-08 22:09:10 -0500

    Version 1.1.1

--- src/categoricalsyllogism/package-info.java
+++ src/categoricalsyllogism/package-info.java
@@ -4,8 +4,8 @@
  * 8 February 2017 <br>
  * <br>
  * Randomly generates a categorical syllogism and the three letters and one number which describes it.&nbsp;It also
- * states whether the syllogism is validSyllogism or invalid.&nbsp;As such, if a fallacy is committed, the fallacy will be red,
- * if the fallacy is not committed, it will be green.&nbsp;It is also possible to add statements which will be randomly
+ * states whether the syllogism is valid or invalid.&nbsp;As such, if a fallacy is committed, the fallacy will be red,
+ * if the fallacy is not committed, it will be green.&nbsp;It is also possible to add words which will be randomly
  * generated in the statements.&nbsp;Last but not least, the program has a venn diagram.&nbsp;Check marks will appear as
  * green, x marks will appear as red, and filled in sections will be filled in with orange.
  */
